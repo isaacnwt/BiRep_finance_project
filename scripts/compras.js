643f15cef5f2324365f6f35cbc31aa6a5d0903b8
Victor_Morbeck <victormorbeck212@gmail.com>
2022-07-11 23:35:54 -0300

    Layout parcialmente definido, criado as funcoes de web storage

---
+++ scripts/compras.js
@@ -0,0 +1,14 @@
+//Primeira função para a coleta de dados
+
+function AddWebSotorage(){
+    
+    //Coletando os valores na dom
+    let nome = document.getElementById("nome").value;
+    let produto = document.getElementById("produto").value;
+    let valor = document.getElementById("valor").value ;
+
+     console.log(nome, produto, valor);
+
+    //Adicionar esses dados a um vetor
+}
+
